af617f2e773be0177e4a428a5f5862f96b853046
Xinran Che <gracexinran@gmail.com>
2019-05-15 23:28:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(array)
+  array.each do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(speakers)
+  speakers.each do |name|
+    puts "Hello #{name}! You'll be assigned to room #{speakers.index(name)}!"
+  end
+end
+  
